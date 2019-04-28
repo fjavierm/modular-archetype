c8182dc0f30fcbe09bacd5dd7c416b836a042037
fjavierm <fco.javier.marquez@gmail.com>
2019-04-28 08:56:45 +0100

    Create maven archetype with dynamic elements

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-one/src/main/java/__classPrefix__OneApp.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $smbol_escape = '\' )
+package ${package}.one;
+
+public class ${classPrefix}OneApp {
+}
---
+++ src/main/resources/archetype-resources/__rootArtifactId__-two/src/main/java/__classPrefix__TwoApp.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $smbol_escape = '\' )
+package ${package}.two;
+
+public class ${classPrefix}TwoApp {
+}
