243794b9662d9ae9377ff29416e303dfdd317631
fjavierm <fco.javier.marquez@gmail.com>
2019-04-30 19:31:24 +0100

    Fix test

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -3,4 +3,8 @@ import groovy.io.FileType
 def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
 def oneBundle = new File(rootDir, request.getArtifactId() + "-one")
 
-assert new File(oneBundle, "src/main/java/" + "toDelete.txt").delete()
+def projectPackage = request.getProperties().get("package")
+
+assert new File(oneBundle, "src/main/java/" 
+    + projectPackage.split("\\.").join('/')
+    + "/toDelete.txt").delete()
